e1fa33072ab898bdc83969c750453b48757678d7
Eslam Medhat <28203059+madahetooo@users.noreply.github.com>
2021-01-20 22:52:28 +0200

    Adding startup name generator clone

--- mega_flutter_app/lib/home.dart
+++ mega_flutter_app/lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';  // Add this line.
 
 class home extends StatefulWidget{
   @override
@@ -10,6 +11,41 @@ class home extends StatefulWidget{
 
 class _homeState extends State<home> {
   int _selectedIndex = 0;
+  final List<WordPair> _suggestions = <WordPair>[];
+  final _saved = Set<WordPair>();     // NEW
+
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18); // NEW
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        // NEW lines from here...
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+                (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        }, // ...to here.
+      ),
+    );
+  }
 
   void _onItemTapped(int index) {
     setState(() {
@@ -17,33 +53,98 @@ class _homeState extends State<home> {
     });
   }
 
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    Widget _buildRow(WordPair pair) {
+      final alreadySaved = _saved.contains(pair);  // NEW
+
+      return ListTile(
+        title: Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        ),
+        trailing: Icon(   // NEW from here...
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null,
+        ),
+        onTap: (){
+          setState(() {
+            if(alreadySaved){
+              _saved.remove(pair);
+            }
+            else{
+              _saved.add(pair);
+            }
+          });
+        },
+      );
+    }
+// NEW
+    Widget _buildSuggestions() {
+      return ListView.builder(
+          padding: const EdgeInsets.all(16),
+          // The itemBuilder callback is called once per suggested
+          // word pairing, and places each suggestion into a ListTile
+          // row. For even rows, the function adds a ListTile row for
+          // the word pairing. For odd rows, the function adds a
+          // Divider widget to visually separate the entries. Note that
+          // the divider may be difficult to see on smaller devices.
+          itemBuilder: (BuildContext _context, int i) {
+            // Add a one-pixel-high divider widget before each row
+            // in the ListView.
+            if (i.isOdd) {
+              return Divider();
+            }
+
+            // The syntax "i ~/ 2" divides i by 2 and returns an
+            // integer result.
+            // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+            // This calculates the actual number of word pairings
+            // in the ListView,minus the divider widgets.
+            final int index = i ~/ 2;
+            // If you've reached the end of the available word
+            // pairings...
+            if (index >= _suggestions.length) {
+              // ...then generate 10 more and add them to the
+              // suggestions list.
+              _suggestions.addAll(generateWordPairs().take(10));
+            }
+            return _buildRow(_suggestions[index]);
+          }
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('G-mail Demo'),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: RaisedButton(
-            color: Colors.red,
-            onPressed: () {
-              // Navigator.pop(context);
-            },
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Text('Show Alert Dialog', style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white
-              ),),
-            ),
+        title: Text('Startup Name Generator'),
+        actions: [
+          IconButton(icon: Icon(Icons.list_alt), onPressed: _pushSaved),
 
-          ),
-        ),
+        ],
       ),
+      body: _buildSuggestions(),
+        // child: Text(wordPair.asPascalCase),
+        // child: Padding(
+        //   padding: const EdgeInsets.all(20.0),
+        //   child: RaisedButton(
+        //     color: Colors.red,
+        //     onPressed: () {
+        //       // Navigator.pop(context);
+        //     },
+        //     child: Padding(
+        //       padding: const EdgeInsets.all(20.0),
+        //       child: Text('Show Alert Dialog', style: TextStyle(
+        //           fontSize: 30,
+        //           fontWeight: FontWeight.bold,
+        //           color: Colors.white
+        //       ),),
+        //     ),
+        //
+        //   ),
+        // ),
+
       drawer: Drawer(
         elevation: 20,
         child: Column(
